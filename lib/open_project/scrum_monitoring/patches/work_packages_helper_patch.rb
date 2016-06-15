983ef841e66a1e1a0ebbc02fa7bc36abc6e73c72
almeidaah <almeidascvn@gmail.com>
2016-06-15 14:02:27 -0300

    Adicao campo totaltasksize e alteracoes relatorios(roadmap)

--- lib/open_project/scrum_monitoring/patches/work_packages_helper_patch.rb
+++ lib/open_project/scrum_monitoring/patches/work_packages_helper_patch.rb
@@ -12,14 +12,6 @@ module OpenProject::ScrumMonitoring::Patches::WorkPackagesHelperPatch
          ################################################################################
          ################################################################################
          
-         def estimated_lines(estimated_hours, complexity, subtype,  context_id)
-            @context = Context.where(:id => context_id)
-            productivity_average = @context[0].productivity_average.to_f
-            calculate(calculated_estimated_time(estimated_hours, complexity, subtype, context_id), productivity_average) do 
-              calculated_estimated_time(estimated_hours, complexity, subtype, context_id) * productivity_average
-            end 
-         end
-
          def calculated_estimated_time(estimated_hours, complexity, subtype,  context_id) 
             return estimated_hours * correction_factor(complexity, subtype, context_id)
          end
@@ -42,7 +34,7 @@ module OpenProject::ScrumMonitoring::Patches::WorkPackagesHelperPatch
 
          def calculated_lines_percentage(commited_lines, estimated_lines)    
             calculate(commited_lines, estimated_lines) do 
-              (commited_lines.fdiv(estimated_lines) * 100.0).floor 
+              (commited_lines.fdiv(estimated_lines) * 100.0)
             end           
           end
 
@@ -67,25 +59,43 @@ module OpenProject::ScrumMonitoring::Patches::WorkPackagesHelperPatch
             @context = Context.where(:id => @groupversion[0].context_id)
             return work_package.estimated_hours * @context[0].productivity_average.to_f
         end
-         def inspection_preparation_time(work_package)
-          @context = Context.where(:id => @groupversion[0].context_id)
-          calculate(ticket_size(work_package), @context[0].inspection_preparation_average.to_f) do 
-            return (ticket_size(work_package) / @context[0].inspection_preparation_average.to_f) * 60 * 60 
+
+        def inspection_preparation_time(work_package)
+          ticket_size_lines = ticket_size(work_package) 
+          inspection_preparation_average_value = context[0].inspection_preparation_average.to_f
+          calculate(ticket_size_lines , inspection_preparation_average_value) do 
+            return (ticket_size_lines / inspection_preparation_average_value)
           end 
         end
 
         def inspection_time(work_package)
-          @context = Context.where(:id => @groupversion[0].context_id)
-          calculate(ticket_size(work_package), @context[0].inspection_duration_average.to_f) do 
-              return (ticket_size(work_package) / @context[0].inspection_duration_average.to_f) * 60 * 60 
+          ticket_size_lines = ticket_size(work_package)
+          inspection_duration_average_value = context[0].inspection_duration_average.to_f
+          calculate(ticket_size_lines, inspection_duration_average_value) do 
+              return (ticket_size_lines / inspection_duration_average_value)
           end 
         end
 
         def rework_time(work_package)
-          @context = Context.where(:id => @groupversion[0].context_id)
-          calculate(ticket_size(work_package), @context[0].rework_average.to_f) do 
-              return (ticket_size(work_package) / @context[0].rework_average.to_f) * 60 * 60 
+          ticket_size_lines = ticket_size(work_package)
+          rework_average_value = context[0].rework_average.to_f
+          calculate(ticket_size_lines, rework_average_value) do 
+              return (ticket_size_lines / rework_average_value)
           end 
+        end
+
+        def total_time(work_package)
+          customFieldValues = CustomValue.where(:customized_id => work_package.id)
+          context_id = @groupversion[0].context_id
+          @context = Context.where(:id => context_id)
+
+          
+          complexity = customFieldValues[1]
+          subtype = customFieldValues[0]
+
+          total_hours = work_package.estimated_hours*correction_factor(complexity, subtype, context_id)
+          
+          return total_hours + inspection_preparation_time(work_package) + inspection_time(work_package) + rework_time(work_package)
         end
 
         def calculate(x, y)
@@ -102,6 +112,10 @@ module OpenProject::ScrumMonitoring::Patches::WorkPackagesHelperPatch
             return cf[0].value
           end
             return 0
+        end
+
+        def context
+          Context.where(:id => @groupversion[0].context_id)
         end
 
     end
